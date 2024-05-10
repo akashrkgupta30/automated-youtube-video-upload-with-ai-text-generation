1a687d51abc249012d9e4b16d06ad49872f1f0b8
Akash Gupta <53559214+akashrkgupta30@users.noreply.github.com>
2024-05-10 23:33:48 +0530

    Delete open_ai_config.py

--- open_ai_config.py
+++
@@ -1,3 +0,0 @@
-api_key="<api-key>"
-description="Write YouTube video description for {book_name} verse {verse_number} chapter {chapter} in 200 characters."
-tags="Write YouTube video tags for {book_name} verse {verse_number} chapter {chapter} in 200 characters."
